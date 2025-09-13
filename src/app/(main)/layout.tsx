8d83eaaf49badd7aa3015716cebbc9788133f4fe
EDUARDO IRIARTE ALVAREZ <edoiriarte@gmail.com>
2025-09-13 20:06:36 +0000

    aplicar este formato a la aplicacion, a los dashboar, infores y presenta

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -31,7 +31,7 @@ export default function MainLayout({
         </SidebarFooter>
       </Sidebar>
       <SidebarInset>
-        <header className="flex h-14 items-center gap-4 border-b bg-card px-6">
+        <header className="flex h-14 items-center gap-4 border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-6 sticky top-0 z-30">
           <SidebarTrigger className="md:hidden" />
           <div className="flex-1">
             <h1 className="text-lg font-semibold md:text-xl font-headline"></h1>
