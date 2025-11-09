67dd8af4b5b4fe23a09e6dd75d7cc43ce5282805
EDUARDO IRIARTE ALVAREZ <edoiriarte@gmail.com>
2025-11-09 22:39:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1617125356-dde41",
+  "appId": "1:856243097775:web:42e472ad506fe5766f845a",
+  "apiKey": "AIzaSyBg9y45BP2hD4CIVxRujKpMiapWXt-yyDE",
+  "authDomain": "studio-1617125356-dde41.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "856243097775"
+};
