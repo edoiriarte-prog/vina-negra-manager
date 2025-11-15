d4e06ebf1fa88ac108f333057371a07b9c3678b4
EDUARDO IRIARTE ALVAREZ <edoiriarte@gmail.com>
2025-11-15 11:59:00 +0000

    que se hace ahora para que inicie sin problemas?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,33 +32,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <FirebaseClientProvider>
-          <AuthGuard>
-            <SidebarProvider>
-              <Sidebar className="print:hidden">
-                <SidebarHeader className="p-0">
-                  <Logo />
-                </SidebarHeader>
-                <SidebarContent className="p-0">
-                  <div className="flex flex-col h-full p-3">
-                    <MainNav />
-                  </div>
-                </SidebarContent>
-                <SidebarFooter>
-                  <Separator className="my-2" />
-                  <UserNav />
-                </SidebarFooter>
-              </Sidebar>
-              <SidebarInset>
-                <header className="flex h-14 items-center gap-4 border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-6 sticky top-0 z-30 print:hidden">
-                  <SidebarTrigger className="md:hidden" />
-                  <div className="flex-1">
-                    <h1 className="text-lg font-semibold md:text-xl"></h1>
-                  </div>
-                </header>
-                <main className="flex-1 overflow-auto p-4 md:p-6 print:p-0">{children}</main>
-              </SidebarInset>
-            </SidebarProvider>
-          </AuthGuard>
+            {children}
         </FirebaseClientProvider>
         <Toaster />
       </body>
