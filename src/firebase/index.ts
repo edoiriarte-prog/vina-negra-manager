6df451797e18f0214fdee170b9387797c0bbf7b4
EDUARDO IRIARTE ALVAREZ <edoiriarte@gmail.com>
2025-12-03 11:15:39 +0000

    "He sobrescrito el archivo src/firebase/index.ts y he borrado accidental

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,15 +5,7 @@
 import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getFirestore, Firestore } from "firebase/firestore";
 import { getAuth, Auth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+import { firebaseConfig } from "./config";
 
 function initializeFirebase(): { firebaseApp: FirebaseApp; firestore: Firestore; auth: Auth } {
   const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
@@ -32,7 +24,6 @@ export { FirebaseClientProvider } from './client-provider';
 
 // Export auth-related hooks and functions
 export { useUser } from './provider';
-export { initiateAnonymousSignIn, initiateEmailSignUp, initiateEmailSignIn, signOut } from './non-blocking-login';
 
 // Export Firestore-related hooks and functions
 export { useCollection } from './firestore/use-collection';
